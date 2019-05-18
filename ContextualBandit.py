ba25280717e76491b87df4e1887663f21409c766
GlebGlushko <gleb.glushko10@gmail.com>
2019-05-18 21:17:30 +0300

    added class agent

--- ContextualBandit.py
+++ ContextualBandit.py
@@ -21,6 +21,26 @@ class contextual_bandit():
         return 1 if result > bandit else -1
 
 
+class agent():
+    def __init__(self, learning_rate, s_size, a_size):
+        self.state_in = tf.placeholder(shape=[1], dtype=tf.int32)
+        state_in_OH = slim.one_hot_encoding(self.state_in, s_size)
+        output = slim.fully_connected(state_in_OH,
+                                      a_size,
+                                      biases_initializer=None,
+                                      activation_fn=tf.nn.sigmoid,
+                                      weights_initializer= tf.ones_initializer())
+        self.output = tf.reshape(output,[-1])
+        self.chosen_action = tf.argmax(self.output,0)
+
+        self.reward_holder = tf.placeholder(shape=[1], dtype=tf.float32)
+        self.action_holder = tf.placeholder(shape=[1], dtype=tf.int32)
+        self.responsible_weight = th.slice(self.output, self.action_holder, [1])
+        self.loss = - (tf.log(self.responsible_weight) * self.reward_holder)
+        optimizer = tf.train.GradientDescentOptimizer(learning_rate=learning_rate)
+        self.update = optimizer.minimize(self.loss)
+
+
 
 
 
